7ea4c3af668efb8e9941c8e185ffb7817fd83fa5
progmiya <techcamp@PC-7743.local>
2013-08-21 17:11:45 +0900

    テスト

--- Classes/TashouNoEnViewController.m
+++ Classes/TashouNoEnViewController.m
@@ -1,10 +1,3 @@
-//
-//  TashouNoEnViewController.m
-//  TashouNoEn
-//
-//  Created by 上田 澄博 on 09/08/25.
-//  Copyright __MyCompanyName__ 2009. All rights reserved.
-//
 
 #import "TashouNoEnViewController.h"
 
