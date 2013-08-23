fa8d6f4c8a5e627c2ef1c47f38e4f1d49f3afd40
progmiya <techcamp@PC-7743.local>
2013-08-23 15:29:05 +0900

    Revert "サーバーサイドコード実装"
    This reverts commit b7203270ef70d8bf6f23536dc7504762506195dc.

--- InfectionServer/index.php
+++ InfectionServer/index.php
@@ -2,32 +2,11 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Infection テストページ</title>
+        <title></title>
     </head>
     <body>
-        <h1>ウィルス拡散開始</h1>
-        <form action="spread.php" method="post">
-            <p>virus_id</p>
-            <input type="text" name="virus_id" />
-            <input type="submit"/>
-        </form>
-        <h1>ウィルス感染</h1>
-        <form action="infected.php" method="post">
-            <p>virus_id</p>
-            <input type="text" name="virus_id" />
-            <input type="submit"/>
-        </form>
-        <h1>ウィルス復帰</h1>
-        <form action="recovered.php" method="post">
-            <p>virus_id</p>
-            <input type="text" name="virus_id" />
-            <input type="submit"/>
-        </form>
-        <h1>ウィルス拡散状況</h1>
-        <form action="state.php" method="post">
-            <p>virus_id</p>
-            <input type="text" name="virus_id" />
-            <input type="submit"/>
-        </form>
+        <?php
+        // put your code here
+        ?>
     </body>
 </html>
--- InfectionServer/recovered.php
+++ InfectionServer/recovered.php
@@ -1,38 +1,7 @@
 <?php
-/**
- * ウィルス感染から復帰したことを通知する
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
-
-require_once 'util.php';
-
-$required_keys = array('virus_id');
-assert_given_post($required_keys);
-$virus_id = $_POST['virus_id'];
-
-// ウィルスの感染数を増やすクエリ
-$decrement_query = "UPDATE InfectionState
-                 SET infected_now   = infected_now -1
-                 WHERE virus_id = $virus_id";
-
-// DBへ接続
-$link = connect_or_exit();
-
-// クエリを発行して感染数を減少させる
-// 更新が衝突しないように、トランザクションを使って行レベルのロックを取得
-mysql_query('START TRANSACTION', $link);
-$success = mysql_query($decrement_query, $link);
-if ($success === FALSE) { // クエリ実行失敗。エラーを吐いて終了
-    mysql_query('ROLLBACK', $link);
-    $json_array = array(
-        'result'  => 'failure',
-        'message' => mysql_error($link),
-    );
-    exit_with_json_array($json_array);
-}
-
-mysql_query('COMMIT', $link);
-$json_array = array(
-    'result'  => 'success',
-    'message' => 'recovered complete'
-);
-exit_with_json_array($json_array);
+?>
