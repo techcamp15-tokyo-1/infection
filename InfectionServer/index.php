8974b235d282619cda1d8def55664648f0e73701
progmiya <techcamp@PC-7743.local>
2013-08-23 15:29:19 +0900

    Revert "Revert "サーバーサイドコード実装""
    This reverts commit fa8d6f4c8a5e627c2ef1c47f38e4f1d49f3afd40.

--- InfectionServer/index.php
+++ InfectionServer/index.php
@@ -2,11 +2,32 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Infection テストページ</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <h1>ウィルス拡散開始</h1>
+        <form action="spread.php" method="post">
+            <p>virus_id</p>
+            <input type="text" name="virus_id" />
+            <input type="submit"/>
+        </form>
+        <h1>ウィルス感染</h1>
+        <form action="infected.php" method="post">
+            <p>virus_id</p>
+            <input type="text" name="virus_id" />
+            <input type="submit"/>
+        </form>
+        <h1>ウィルス復帰</h1>
+        <form action="recovered.php" method="post">
+            <p>virus_id</p>
+            <input type="text" name="virus_id" />
+            <input type="submit"/>
+        </form>
+        <h1>ウィルス拡散状況</h1>
+        <form action="state.php" method="post">
+            <p>virus_id</p>
+            <input type="text" name="virus_id" />
+            <input type="submit"/>
+        </form>
     </body>
 </html>
